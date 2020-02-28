12efdec8cf81393176b44a89494e1270ff29413e
bdiniscia <bdiniscia@gmail.com>
2020-02-28 00:02:07 -0300

    Comienzo del CSS de la vista del mesero

---
+++ src/sections/clientID.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class clientID extends Component {
+    render() {
+        return (
+            <div className='clientID'>
+
+            </div>
+        )
+    }
+
+}
+
+export default clientID;
---
+++ src/sections/orderStats.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class OrderStats extends Component {
+    render() {
+        return (
+            <div className='orderStats'>
+
+            </div>
+        )
+    }
+
+}
+
+export default OrderStats;
