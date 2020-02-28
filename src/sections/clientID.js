8f61742e22d2d3a15504f63888e2ef794fa62d23
Corina <corinacvp@gmail.com>
2020-02-28 12:31:44 -0300

    created template

---
+++ src/sections/clientID.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class clientID extends Component {
+    render() {
+        return (
+            <div className='clientID'>
+                <p>Cliente ID</p>
+            </div>
+        )
+    }
+
+}
+
+export default clientID;
