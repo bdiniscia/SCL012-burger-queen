cffe6ec0a6f3f38fa284cdb2e1dbeb500f5d3980
Corina <corinacvp@gmail.com>
2020-03-05 13:56:48 -0300

    style clientID

--- src/sections/waitress/clientID.js
+++ src/sections/waitress/clientID.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import '../../App.css';
+import './clientID.css'
+import backButton from '../../imagenes/backButton.png'
 
 class clientID extends Component {
     render() {
         return (
             <div className='clientID'>
-                <p>Nombre del Cliente:</p>
+                <div className='client'>
+
+                <div className='button-back'>
+                <img
+                alt='backButton'
+                src={backButton}/>
+
+                <p className='titleClient' >Nombre del Cliente:</p>
+                <input className='inputClient'></input>
+                </div>
+               
+                </div>
+               
             </div>
         )
     }
