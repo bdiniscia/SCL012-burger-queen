cf6f136e4cc2f717d2f184ab2413cd202b28fe8b
Bárbara Di Niscia <bdiniscia@gmail.com>
2020-03-05 09:07:59 -0300

    style welcome (#23)

--- src/templates/welcome.js
+++ src/templates/welcome.js
@@ -1,18 +1,37 @@
-import React, { Component } from "react";
-import Button from "../components/button";
+import React, { Component, Fragment } from "react";
 import { Link } from "react-router-dom";
+import Button from "../components/button";
+import logo from "../imagenes/logo.png";
+import "./welcome.css";
 
 class welcome extends Component {
   render() {
     return (
-      <div>
-        <Link to="/waitress">
-          <Button title="Mesonero" />
-        </Link>
-        <Link to="/cook">
-          <Button title="Cocinero" />
-        </Link>
-      </div>
+      <Fragment>
+        <div className="container-custom">
+          <div className="logo">
+			<img 
+			alt="logo" 
+			src={logo} />
+			
+            <h1 className="title-custom">¿Cuál es tu rol?</h1>
+
+            <div>
+              <div className='button-welcome'>
+			  <Link to="/waitress">
+                <Button  title="Mesonero" />
+              </Link>
+			  </div>
+			  <div className='button-welcome'>
+              <Link to="/cook">
+                <Button  title="Cocinero" />
+              </Link>
+			  </div>
+            </div>
+
+          </div>
+        </div>
+      </Fragment>
     );
   }
 }
