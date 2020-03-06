c1ae75b7241d55b4e870fc67001e77309b768196
Corina Varas <56646549+CorinaVaras@users.noreply.github.com>
2020-03-06 20:48:16 -0300

    agregando select de numero de mesa (#27)
    * agg imagen back button
    
    * style clientID
    
    * final styles of the clientID component
    
    * style clientID

--- src/sections/waitress/clientID.js
+++ src/sections/waitress/clientID.js
@@ -3,7 +3,15 @@ import '../../App.css';
 import './clientID.css'
 import backButton from '../../imagenes/backButton.png'
 
+
 class clientID extends Component {
+
+    handleChange = (e) => {
+        const cliente = e.target.value
+        console.log(cliente)  
+    }
+
+
     render() {
         return (
             <div className='clientID'>
@@ -14,8 +22,23 @@ class clientID extends Component {
                 alt='backButton'
                 src={backButton}/>
 
-                <p className='titleClient' >Nombre del Cliente:</p>
-                <input className='inputClient'></input>
+                <p className='titleClient'>Nombre del Cliente:</p>
+                <input onChange={(e) => this.handleChange(e)} className='inputClient'></input>
+                <p className='titleClient'>Mesa:</p>
+
+                <select className='mesa'>
+                    <option selected>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                    <option>7</option>
+                    <option>8</option>
+                    <option>9</option>
+                    <option>10</option>
+                </select>
+
                 </div>
                
                 </div>
