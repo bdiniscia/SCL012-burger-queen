29e24f343397fe050c9fc17c441f3b20610420c2
Bárbara Di Niscia <bdiniscia@gmail.com>
2020-03-08 19:21:39 -0300

    Dado evento al item del menu. Issue #9 (#31)
    * Estilos de menú en progreso
    
    * Cambios en el json y la forma de renderear el menú
    
    * Dado evento al item del menu. Issue #9

--- src/sections/waitress/menu.js
+++ src/sections/waitress/menu.js
@@ -3,12 +3,30 @@ import '../../App.css';
 import './menu.css'
 import Button from '../../components/button'
 import meals from '../../data.json';
+import Modal from 'react-bootstrap/Modal'
 
+
+// Componente que muestra el menú dependiendo si es breakfast o dinner
 class MenuList extends Component {
+  
+
+  // Clicks en los items del menú
+  handleClick = (e, meal) => {
+    e.preventDefault()
+    console.log(`> meal:`, meal);
+    // Si no tiene opciones para personalizar el producto
+    if (typeof meal.options === 'undefined') {
+      return alert('No tiene options');
+    }
+
+    return alert('Sí tiene opciones');
+  }
+
   render() {
+    // Mapea y muestra cada uno de los elementos del array escogido (Breakfast o dinner)
     const meals = this.props.meals.map((meal) => {
       return (
-        <div key={meal.id} className='itemsMenu'>
+        <div key={meal.id} className='itemsMenu' onClick={(e) => this.handleClick(e, meal)}>
           <p className='itemName'>{meal.name}</p>
           <p className='itemPrice'>${meal.price}</p>
         </div>
@@ -16,6 +34,7 @@ class MenuList extends Component {
     });
 
     return (
+      // Se muestra la constante Meals que se declara arriba
       <div className='itemDiv'>
         { meals }
       </div>
@@ -23,12 +42,17 @@ class MenuList extends Component {
   }
 }
 
+
+// Componente principal de menú
 class Menu extends Component {
+  // Flags para mostrar o ocultar los menús
   state = {
     visibleBreakfast: false,
     visibleDinner: false
   }
 
+  // Filtra los elemntos de la data de acuerdo al prop que se le pase (breakfast o dinner)
+  // y te deja la data filtrada para manipularla en MenuList
   getMealsByType(type) {
     return meals.filter((meal) => {
       return meal.type === type;
@@ -36,16 +60,16 @@ class Menu extends Component {
   }
 
   render () {
+    // Cambios en los flags de acuerdo al botón
     const _renderBreakfast = () => {
       this.setState({
         visibleBreakfast: !this.state.visibleBreakfast, 
         visibleDinner: false})
     }
-
     const _renderDinner = () => {
       this.setState({
-        visibleDinner: !this.state.visibleDinner, 
-        visibleBreakfast: false})
+        visibleBreakfast: false,
+        visibleDinner: !this.state.visibleDinner})
     }
 
     return (
