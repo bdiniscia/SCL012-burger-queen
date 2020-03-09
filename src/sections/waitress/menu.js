036690c3a873e4e35433e66bcfc4b969dc21bdf2
Bárbara Di Niscia <bdiniscia@gmail.com>
2020-03-09 11:58:53 -0300

    Cambios en los estilos de Menú y Total (Ancho en porcentajes) (#35)
    * Estilos de menú en progreso
    
    * Cambios en el json y la forma de renderear el menú
    
    * Dado evento al item del menu. Issue #9
    
    * Issue #10, WIP
    
    * Cambios en CSS

--- src/sections/waitress/menu.js
+++ src/sections/waitress/menu.js
@@ -7,6 +7,7 @@ import Modal from "react-bootstrap/Modal";
 
 // Componente que muestra el menú dependiendo si es breakfast o dinner
 class MenuList extends Component {
+
   // Clicks en los items del menú
   handleClick = (e, meal) => {
     e.preventDefault();
