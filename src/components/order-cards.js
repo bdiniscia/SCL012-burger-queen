e3f4290d90cb019a9cce121836b42dc5fd69c43f
bdiniscia <bdiniscia@gmail.com>
2020-03-12 13:53:48 -0300

    Estilos de la cards de pedidos

--- src/components/order-cards.js
+++ src/components/order-cards.js
@@ -2,25 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types'
 import './order-cards.css'
 
-const OrderCards = ({name, table, orderName, orderOption, state, orderExtras, delivered, onClick}) => {
+const OrderCards = ({name, table, order, state, delivered, onClick}) => {
     return (
         <div
-            className = 'order-cards'
-            onClick = {onClick}>
-                <div className='identification'>
-                    <p>{name}</p>
-                    <p>Mesa: {table}</p>
-                </div>
+        className = 'order-cards'
+        onClick= {onClick} >
+            <div className='identification'>
+                <p>{name}</p>
+                <p>Mesa: {table}</p>
+            </div>
+            <div>
+              {order.map((item) => {
+                return (
                 <div>
-                    <p>{orderName}</p>
-                    <p>{orderOption}</p>
-                    <p>{orderExtras}</p>
+                <p className='item mainItem'>{item.name}</p>
+                <p className='item subMenus'>{item.option}</p>
+                <p className='item subMenus'>{item.extras}</p>
                 </div>
-                <div className='status'>
-                    <p>{state}</p>
-                    <p>{delivered}</p>
-                </div>
-        </div>
+                )
+              })}
+            </div>
+            <div className='status'>
+                <p>{state}</p>
+                <p>{delivered}</p>
+            </div>
+    </div>
     )
 }
 
